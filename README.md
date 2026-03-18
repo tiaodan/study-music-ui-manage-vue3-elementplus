523aeba7857e89c197fa8fa994eef6b55d8c26cb
tiaodan <tiaodanbang@gmail.com>
2026-03-18 18:24:40 +0800

    v0.1 原项目上传，备份

---
+++ README.md
@@ -0,0 +1,37 @@
+项目：歌曲后台管理界面
+思路: 
+在原项目上改，增加自己的需求
+
+安装方式：
+# music-manage
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+
+注意：
+1. node_moudles打包成 zip文件，下载后解压才能用上传此文件，为了以后改动过程种，npm装的插件有问题了，可以回到最初版本还能运行
+2. 要搭配后端程序使用，参考项目：还没实现，没上传github
+3. 参考项目：https://github.com/Yin-Hongwei/music-website  的 music-manage目录
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+	
+	// ←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+  // 这里才是控制 devServer 的地方！！
+  devServer: {
+    //host: '127.0.0.1',           // 关键！允许外部访问0.0.0.0（包括 Caddy）
+    host: '0.0.0.0',           // 关键！允许外部访问0.0.0.0（包括 Caddy）
+    port: 9002,                // 你现在用的端口,一般ui-mange 用8002
+    allowedHosts: 'all',       // Vue CLI 4+ 推荐写法（允许所有域名）
+    // allowedHosts: ['www.xx.com', '.xx.com', 'localhost'], // 或者只允许指定域名
+  },
+  // ←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
+	
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      Object.assign(definitions[0]['process.env'], {
+        NODE_HOST: '"http://localhost:8003"',
+      });
+      return definitions;
+    });
+  }
+})
